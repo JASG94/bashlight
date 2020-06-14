e19fab88e9c58cdb90451a819f166fcc809d7961
JASG94 <javiersalvadorgarcia94@gmail.com>
2020-06-14 13:31:53 +0200

    First working version

---
+++ worklight.py
@@ -0,0 +1,43 @@
+import time
+import magichue
+import sys
+
+light_ip="192.168.1.64"
+
+light = magichue.Light(light_ip) 
+colors = {
+    "red": (255,0,0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "purple": (255, 0, 255),
+    "yellow": (255, 255, 0)
+}
+
+
+if len(sys.argv) <= 2:
+    if sys.argv[1] == "on":
+        light.is_white = True
+        light.brightness = 255
+
+    elif sys.argv[1] == "off":
+        light.on=False
+
+else:
+    if sys.argv[2].isdigit():
+        if sys.argv[1] == "on":
+            light.is_white = True
+            light.brightness = int(round(((float(sys.argv[2])/100)*255),0))
+
+        elif sys.argv[1] == "off":
+            light.on=False
+    else:
+        if sys.argv[1] == "on":
+            light.is_white = False
+            light.rgb = (colors[sys.argv[2]])
+            light.brightness = 255
+
+        elif sys.argv[1] == "off":
+            light.on = False 
+    
+
+
---
+++ worklight.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3.8 ./worklight.py $1 $2
